e02737eec4dd02762141f360fbed04901eb9bb85
kamal <kamal@neptune.(none)>
2013-02-08 15:50:03 +0530

    Finish user edit, update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,21 @@
 module UsersHelper
-def gravatar_for(user)
-    gravatar_id = user.id
-    gravatar_url = "#{gravatar_id}.jpg"
+  
+  def gravatar_for(user, options = { size: 50 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+  
+  
+  
+  
+  
+# def gravatar_for(user)
+    # gravatar_id = user.id
+    # gravatar_url = "#{gravatar_id}.jpg"
+    # image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  # end
 
 
 end
